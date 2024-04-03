Adding the final integrated gradient, Fixing the interchanged dual output result, experimenting with batchnorm and dropout

train_test_batchnormdropout.py
@@ -177,7 +177,7 @@ if __name__=="__main__":
 
     from wrapper.data_setup import SequenceDataset
     from wrapper.utils import plot_loss_function, plot_correlation, seed_everything
-    from model import NaiveModelV1, NaiveModelV2, NaiveModelV3, MultiRMModel, ConvTransformerModel, ConfigurableModel
+    from model import ConfigurableModelWoBatchNormDropout, NaiveModelV2, NaiveModelV2WoBatchNormDropOut, NaiveModelV3, MultiRMModel, ConvTransformerModel, ConfigurableModel
     import sys
 
     # Set logging template
@@ -316,16 +316,16 @@ if __name__=="__main__":
             logging.info(f"Input size: {input_size}")
             if m6A_info=="level_channel" or m6A_info=="flag_channel": 
                 input_channel = 5
-                # model = NaiveModelV2(input_channel=input_channel, cnn_first_filter=8, input_size=input_size)
+                model = NaiveModelV2(input_channel=input_channel, cnn_first_filter=8, input_size=input_size)
                 # model = ConvTransformerModel(input_channel=input_channel)
                 # model = MultiRMModel(1, True)
-                model = ConfigurableModel(input_channel=input_channel, input_size=input_size, cnn_first_filter=12, cnn_first_kernel_size=9, cnn_length=2, cnn_other_filter=64, cnn_other_kernel_size=3, bilstm_layer=3, bilstm_hidden_size=64, fc_size=256)
+                # model = ConfigurableModelWoBatchNormDropout(input_channel=input_channel, input_size=input_size, cnn_first_filter=12, cnn_first_kernel_size=9, cnn_length=2, cnn_other_filter=64, cnn_other_kernel_size=3, bilstm_layer=3, bilstm_hidden_size=64, fc_size=256)
             else:
                 input_channel = 4
-                # model = NaiveModelV2(input_channel=input_channel, cnn_first_filter=8, input_size=input_size)
+                model = NaiveModelV2(input_channel=input_channel, cnn_first_filter=8, input_size=input_size)
                 # model = ConvTransformerModel(input_channel=input_channel)
                 # model = MultiRMModel(1, True)
-                model = ConfigurableModel(input_channel=input_channel, input_size=input_size, cnn_first_filter=12, cnn_first_kernel_size=9, cnn_length=2, cnn_other_filter=64, cnn_other_kernel_size=3, bilstm_layer=3, bilstm_hidden_size=64, fc_size=256)
+                # model = ConfigurableModelWoBatchNormDropout(input_channel=input_channel, input_size=input_size, cnn_first_filter=12, cnn_first_kernel_size=9, cnn_length=2, cnn_other_filter=64, cnn_other_kernel_size=3, bilstm_layer=3, bilstm_hidden_size=64, fc_size=256)
 
             model.to(device)
             #model=torch.nn.DataParallel(model) 

train_test_batchnormdropouttuned.py
@@ -104,7 +104,7 @@ def train(model: torch.nn.Module, train_loader: DataLoader, test_loader: DataLoa
     #     logging.info(f"Set benchmark to True for CUDNN")
     #     torch.backends.cudnn.benchmark = True
     
-    early_stopper = EarlyStopper(patience=50, min_delta=0.01)    
+    early_stopper = EarlyStopper(patience=2, min_delta=0.01)    
     
     for epoch in range(1, epochs+1):    
         model.train()
@@ -177,7 +177,7 @@ if __name__=="__main__":
 
     from wrapper.data_setup import SequenceDataset
     from wrapper.utils import plot_loss_function, plot_correlation, seed_everything
-    from model import NaiveModelV1, NaiveModelV2, NaiveModelV3, MultiRMModel, ConvTransformerModel, ConfigurableModel
+    from model import ConfigurableModelWoBatchNormDropout, NaiveModelV2, NaiveModelV2WoBatchNormDropOut, NaiveModelV3, MultiRMModel, ConvTransformerModel, ConfigurableModel
     import sys
 
     # Set logging template
@@ -253,7 +253,7 @@ if __name__=="__main__":
     logging.info(f"Running on device: {device}")  
     
     # Set seed for everything. Important for reproducibilty see: https://www.kaggle.com/code/bminixhofer/deterministic-neural-networks-using-pytorch
-    seed_everything(445566) # For other model configuration
+    seed_everything(1234) # For other model configuration
     # seed_everything(44) # For model with promoter
     # seed_everything(45) # For m6A_info: flag_channel, add_promoter: True, target: control
     # seed_everything(4455)  # best param 
@@ -319,18 +319,20 @@ if __name__=="__main__":
                 # model = NaiveModelV2(input_channel=input_channel, cnn_first_filter=8, input_size=input_size)
                 # model = ConvTransformerModel(input_channel=input_channel)
                 # model = MultiRMModel(1, True)
-                model = ConfigurableModel(input_channel=input_channel, input_size=input_size, cnn_first_filter=12, cnn_first_kernel_size=9, cnn_length=2, cnn_other_filter=64, cnn_other_kernel_size=3, bilstm_layer=3, bilstm_hidden_size=64, fc_size=256)
+                #TUNED
+                model = ConfigurableModelWoBatchNormDropout(input_channel=input_channel, input_size=input_size, cnn_first_filter=16, cnn_first_kernel_size=5, cnn_length=2, cnn_other_filter=64, cnn_other_kernel_size=7, bilstm_layer=3, bilstm_hidden_size=256, fc_size=256)
             else:
                 input_channel = 4
                 # model = NaiveModelV2(input_channel=input_channel, cnn_first_filter=8, input_size=input_size)
                 # model = ConvTransformerModel(input_channel=input_channel)
                 # model = MultiRMModel(1, True)
-                model = ConfigurableModel(input_channel=input_channel, input_size=input_size, cnn_first_filter=12, cnn_first_kernel_size=9, cnn_length=2, cnn_other_filter=64, cnn_other_kernel_size=3, bilstm_layer=3, bilstm_hidden_size=64, fc_size=256)
+                #TUNED
+                model = ConfigurableModelWoBatchNormDropout(input_channel=input_channel, input_size=input_size, cnn_first_filter=16, cnn_first_kernel_size=5, cnn_length=2, cnn_other_filter=64, cnn_other_kernel_size=7, bilstm_layer=3, bilstm_hidden_size=256, fc_size=256)
 
             model.to(device)
             #model=torch.nn.DataParallel(model) 
             #optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) # here added the weight dacay
-            optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+            optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.05)
             # optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, eps=adam_epsilon, betas=(beta1,beta2))
             #optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay=1e-5) # here added the weight dacay for temp_w_l2reg. for temp no.
             #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer,gamma=0.1) # here added exponenetial dcay for the optimizer

train_test_dual_batchnormdropout.py
@@ -90,7 +90,7 @@ def train(model: torch.nn.Module, train_loader: DataLoader, test_loader: DataLoa
     if not optimizer:
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     
-    early_stopper = EarlyStopper(patience=3, min_delta=0.01)    
+    early_stopper = EarlyStopper(patience=2, min_delta=0.01)    
     
     for epoch in range(1, epochs+1):    
         model.train()
@@ -151,7 +151,7 @@ if __name__=="__main__":
 
     from wrapper.data_setup import SequenceDatasetDual, SequenceDatasetDualGene2Vec
     from wrapper.utils import plot_loss_function, plot_correlation, seed_everything
-    from model import NaiveModelV1, NaiveModelV2, NaiveModelV3, MultiRMModel, ConvTransformerModel, ConfigurableModel
+    from model import NaiveModelV1, NaiveModelV2, NaiveModelV3, MultiRMModel, ConvTransformerModel, ConfigurableModel, ConfigurableModelWoBatchNormDropout
     import sys
 
     # Set logging template
@@ -281,14 +281,14 @@ if __name__=="__main__":
                     # model = NaiveModelV2(input_channel=5, cnn_first_filter=8, input_size=input_size)
                     # model = ConvTransformerModel(input_channel=5)
                     # model = MultiRMModel(1, True)
-                    model = ConfigurableModel(input_channel=5, cnn_first_filter=config["cnn_first_filter"], cnn_first_kernel_size=config["cnn_first_kernel_size"],
+                    model = ConfigurableModelWoBatchNormDropout(input_channel=5, cnn_first_filter=config["cnn_first_filter"], cnn_first_kernel_size=config["cnn_first_kernel_size"],
                                 cnn_length=config["cnn_length"], cnn_other_filter=config["cnn_filter"], cnn_other_kernel_size=config["cnn_kernel_size"], bilstm_layer=config["bilstm_layer"], bilstm_hidden_size=config["bilstm_hidden_size"], fc_size=config["fc_size"],
                                 output_size=2)
                 else:
                     # model = NaiveModelV2(input_channel=4, cnn_first_filter=8, input_size=input_size, output_dim=2)
                     # model = ConvTransformerModel(input_channel=4)
                     # model = MultiRMModel(1, True)
-                    model = ConfigurableModel(input_channel=4, cnn_first_filter=config["cnn_first_filter"], cnn_first_kernel_size=config["cnn_first_kernel_size"],
+                    model = ConfigurableModelWoBatchNormDropout(input_channel=4, cnn_first_filter=config["cnn_first_filter"], cnn_first_kernel_size=config["cnn_first_kernel_size"],
                                 cnn_length=config["cnn_length"], cnn_other_filter=config["cnn_filter"], cnn_other_kernel_size=config["cnn_kernel_size"], bilstm_layer=config["bilstm_layer"], bilstm_hidden_size=config["bilstm_hidden_size"], fc_size=config["fc_size"],
                                 output_size=2)
             if args.embedding=="gene2vec":
@@ -302,7 +302,7 @@ if __name__=="__main__":
 
 
             #optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate) # here added the weight dacay
-            optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon, betas=(args.adam_beta1, args.adam_beta2)) # here added the weight dacay
+            optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate, eps=args.adam_epsilon, betas=(args.adam_beta1, args.adam_beta2), weight_decay=0.05) # here added the weight dacay
             #optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, weight_decay=1e-5) # here added the weight dacay for temp_w_l2reg. for temp no.
             #scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer,gamma=0.1) # here added exponenetial dcay for the optimizer
             # Train and Validate the model

tune.py
@@ -9,17 +9,16 @@ import torch.optim as optim
 from ray import train, tune
 from ray.train import Checkpoint, RunConfig
 from ray.tune.schedulers import ASHAScheduler
-from model import ConfigurableModel
+from model import ConfigurableModel, ConfigurableModelWoBatchNormDropout
 from wrapper.data_setup import SequenceDataset
 from wrapper.utils import seed_everything
 from torch.utils.data import DataLoader
 from wrapper.utils import EarlyStopper
 from torchmetrics.functional.regression import pearson_corrcoef
-import logging
-
+from argparse import ArgumentParser
 
 def train_seq(config, train_loader, test_loader, target): 
-    model = ConfigurableModel(cnn_first_filter=config["cnn_first_filter"], cnn_first_kernel_size=config["cnn_first_kernel_size"],
+    model = ConfigurableModelWoBatchNormDropout(cnn_first_filter=config["cnn_first_filter"], cnn_first_kernel_size=config["cnn_first_kernel_size"],
                               cnn_length=config["cnn_length"], cnn_other_filter=config["cnn_filter"], cnn_other_kernel_size=config["cnn_kernel_size"], bilstm_layer=config["bilstm_layer"], bilstm_hidden_size=config["bilstm_hidden_size"], fc_size=config["fc_size"])
 
     seed_everything(1234)                
@@ -118,7 +117,7 @@ def train_seq(config, train_loader, test_loader, target):
                     "pred": pred.numpy(),
                     "true": true.numpy()}
 
-        logging.info(f"Epoch: {epoch}, train_loss: {train_loss:.7f}, val_loss: {test_loss:.7f}, val_mse:{metrics['mse']:.7f}, val_rmse: {metrics['rmse']:.7f}, val_mae: {metrics['mae']:.7f}, val_pearson_corr: {metrics['pearson_corr']:.7f}")
+        print(f"Epoch: {epoch}, train_loss: {train_loss:.7f}, val_loss: {test_loss:.7f}, val_mse:{metrics['mse']:.7f}, val_rmse: {metrics['rmse']:.7f}, val_mae: {metrics['mae']:.7f}, val_pearson_corr: {metrics['pearson_corr']:.7f}")
 
         with tempfile.TemporaryDirectory() as temp_checkpoint_dir:
             path = os.path.join(temp_checkpoint_dir, "checkpoint.pt")
@@ -132,8 +131,8 @@ def train_seq(config, train_loader, test_loader, target):
             )
 
 
-def main(num_samples=10, max_num_epochs=50, gpus_per_trial=3):
-    seed_everything(4455)
+def main(num_samples=10, max_num_epochs=50, gpus_per_trial=3, args=None):
+    seed_everything(1234)
     config = {
         # Uniform distribuitinon
         "lr": tune.choice([0.5e-2, 1e-3, 1e-2]),
@@ -192,10 +191,10 @@ def main(num_samples=10, max_num_epochs=50, gpus_per_trial=3):
             scheduler=scheduler,
             reuse_actors=False,
             num_samples=num_samples,
-            max_concurrent_trials=7,
+            max_concurrent_trials=5,
         ),
         param_space=config,
-        run_config=RunConfig(storage_path="/binf-isilon/renniegrp/vpx267/ucph_thesis/ray_results/adam", name="test_experiment")
+        run_config=RunConfig(storage_path=args.tune_output_path, name=args.tune_output_folder_name)
     )
     results = tuner.fit()
     
@@ -207,4 +206,26 @@ def main(num_samples=10, max_num_epochs=50, gpus_per_trial=3):
     print("Best trial final validation pearson correlation: {}".format(
         best_result.metrics["pearson_corr"]))
 
-main(num_samples=500, max_num_epochs=15, gpus_per_trial=1)
+if __name__ == "__main__":
+    """
+    EXAMPLE
+    -----
+
+    python tune.py --tune_output_path /binf-isilon/renniegrp/vpx267/ucph_thesis/ray_results --tune_output_folder_name wo_batchnorm_dropout_single_case --num_samples 200 --max_num_epochs 50
+    """
+    parser = ArgumentParser(
+        description="Running Ray Tuning"
+    )
+    parser.add_argument("--embedding", default='one-hot',const='one-hot', nargs='?', 
+                        choices=['one-hot', 'gene2vec'], 
+                        help="Embedding options ['one-hot', 'gene2vec']")
+    parser.add_argument("--embedding_file", default=None, help="Embedding file for gene2vec")
+    parser.add_argument("--tune_output_path", default="ray_results", help="Output path for tune")
+    parser.add_argument("--tune_output_folder_name", default="tuning", help="Output folder name for tune")
+    parser.add_argument("--num_samples", default=50, type=int, help="number of samples for ray tuning")
+    parser.add_argument("--max_num_epochs", default=15, type=int, help="number of samples for ray tuning")
+    args = parser.parse_args()
+
+    print(args)
+
+    main(num_samples=args.num_samples, max_num_epochs=args.max_num_epochs, gpus_per_trial=1, args=args)

tune_dual.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 from ray import train, tune
 from ray.train import Checkpoint, RunConfig
 from ray.tune.schedulers import ASHAScheduler
-from model import ConfigurableModel
+from model import ConfigurableModel, ConfigurableModelWoBatchNormDropout
 from wrapper.data_setup import SequenceDatasetDual, SequenceDatasetDualGene2Vec
 from torchmetrics.wrappers import MultioutputWrapper
 from torchmetrics.regression import PearsonCorrCoef, MeanSquaredError, MeanAbsoluteError
@@ -20,7 +20,7 @@ import logging
 from argparse import ArgumentParser
 
 def train_seq(config, input_channel, input_size, train_loader, test_loader): 
-    model = ConfigurableModel(input_channel=input_channel, input_size=input_size, cnn_first_filter=config["cnn_first_filter"], cnn_first_kernel_size=config["cnn_first_kernel_size"],
+    model = ConfigurableModelWoBatchNormDropout(input_channel=input_channel, input_size=input_size, cnn_first_filter=config["cnn_first_filter"], cnn_first_kernel_size=config["cnn_first_kernel_size"],
                               cnn_length=config["cnn_length"], cnn_other_filter=config["cnn_filter"], cnn_other_kernel_size=config["cnn_kernel_size"], bilstm_layer=config["bilstm_layer"], bilstm_hidden_size=config["bilstm_hidden_size"], fc_size=config["fc_size"],
                               output_size=2)
 
@@ -89,8 +89,8 @@ def train_seq(config, input_channel, input_size, train_loader, test_loader):
                 loss2 = criterion(meth_pred_val[:,1], meth_true_val[:,1])
                 loss = loss1 + loss2
 
-                pred = torch.cat([pred, meth_true_val.cpu().detach()])
-                true = torch.cat([true, meth_pred_val.cpu().detach()])
+                pred = torch.cat([pred, meth_pred_val.cpu().detach()])
+                true = torch.cat([true, meth_true_val.cpu().detach()])
                 
                 # Total of average loss from each samples in a batch. Huberloss reduction is mean by default
                 test_loss += loss.item()
@@ -170,20 +170,20 @@ def main(num_samples=10, max_num_epochs=50, gpus_per_trial=1, args=None):
         embedding_file = f"{args.embedding_file}/split_{i}.model"
     print("Generate Dataset")
     # # Just remove the embedding related argument to tune one-hot
-    # train_dataset = SequenceDatasetDual(seq_fasta_path=seq_fasta_train_path, meta_data_path=meta_data_train_json_path, prom_seq_fasta_path=promoter_fasta_train_path, m6A_info=m6A_info_train_path, m6A_info_path=m6A_info_train_path, transform=args.embedding, path_to_embedding=embedding_file)
-    # test_dataset = SequenceDatasetDual(seq_fasta_path=seq_fasta_test_path, prom_seq_fasta_path=promoter_fasta_test_path,  meta_data_path=meta_data_test_json_path, m6A_info=m6A_info_test_path, m6A_info_path=m6A_info_test_path, transform=args.embedding, path_to_embedding=embedding_file)
+    train_dataset = SequenceDatasetDual(seq_fasta_path=seq_fasta_train_path, meta_data_path=meta_data_train_json_path, prom_seq_fasta_path=promoter_fasta_train_path, m6A_info=m6A_info_train_path, m6A_info_path=m6A_info_train_path, transform=args.embedding, path_to_embedding=embedding_file)
+    test_dataset = SequenceDatasetDual(seq_fasta_path=seq_fasta_test_path, prom_seq_fasta_path=promoter_fasta_test_path,  meta_data_path=meta_data_test_json_path, m6A_info=m6A_info_test_path, m6A_info_path=m6A_info_test_path, transform=args.embedding, path_to_embedding=embedding_file)
     
     # USE BIG WORKER FOR THIS
-    path_to_seq = "data/dual_outputs/motif_fasta_train_SPLIT_1.fasta"
-    hdf_file = "/binf-isilon/renniegrp/vpx267/ucph_thesis/data/dual_outputs/hdf5/gene2vec.hdf5"
-    path_to_prom = "data/dual_outputs/promoter_fasta_test_SPLIT_1.fasta"
-    path_to_meta_data_train = "data/dual_outputs/train_meta_data_SPLIT_1.json"
-    path_to_meta_data_test = "data/dual_outputs/test_meta_data_SPLIT_1.json"
-    path_to_embedding = "data/embeddings/gene2vec/dual_outputs/split_1.model"
-    input_channel = 300
-    input_size = 999
-    train_dataset = SequenceDatasetDualGene2Vec(hdf_file, dataset="train/motif_SPLIT_1",  meta_data_path=path_to_meta_data_train)
-    test_dataset = SequenceDatasetDualGene2Vec(hdf_file, dataset="test/motif_SPLIT_1",  meta_data_path=path_to_meta_data_test)
+    # path_to_seq = "data/dual_outputs/motif_fasta_train_SPLIT_1.fasta"
+    # hdf_file = "/binf-isilon/renniegrp/vpx267/ucph_thesis/data/dual_outputs/hdf5/gene2vec.hdf5"
+    # path_to_prom = "data/dual_outputs/promoter_fasta_test_SPLIT_1.fasta"
+    # path_to_meta_data_train = "data/dual_outputs/train_meta_data_SPLIT_1.json"
+    # path_to_meta_data_test = "data/dual_outputs/test_meta_data_SPLIT_1.json"
+    # path_to_embedding = "data/embeddings/gene2vec/dual_outputs/split_1.model"
+    # input_channel = 300
+    # input_size = 999
+    # train_dataset = SequenceDatasetDualGene2Vec(hdf_file, dataset="train/motif_SPLIT_1",  meta_data_path=path_to_meta_data_train)
+    # test_dataset = SequenceDatasetDualGene2Vec(hdf_file, dataset="test/motif_SPLIT_1",  meta_data_path=path_to_meta_data_test)
     print("Finished Generate Dataset")
     print("Starting dataloader setup")
     train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=5)
@@ -191,7 +191,7 @@ def main(num_samples=10, max_num_epochs=50, gpus_per_trial=1, args=None):
     print("Finished dataloader setup")
     tuner = tune.Tuner(
         tune.with_resources(
-            tune.with_parameters(train_seq, input_channel=300, input_size=999, train_loader=train_loader, test_loader=test_loader),
+            tune.with_parameters(train_seq, input_channel=4, input_size=1001, train_loader=train_loader, test_loader=test_loader),
             resources={"cpu": 1, "gpu": gpus_per_trial}
         ),
         tune_config=tune.TuneConfig(
@@ -214,15 +214,21 @@ def main(num_samples=10, max_num_epochs=50, gpus_per_trial=1, args=None):
         best_result.metrics["loss"]))
     print(f"Best trial final validation control pearson correlation: {best_result.metrics['val_control_pearson_corr']}, case pearson correlation: {best_result.metrics['val_case_pearson_corr']}")
 
+    print(f"Saving the summary into a dataframe... ")
+    results_df = results.get_dataframe()
+    results_df.to_csv(f"{args.tune_output_path}/{args.tune_output_folder_name}/results_summary.csv", index=False)
+
 if __name__ == "__main__":
     """
     EXAMPLE
     -----
 
     python tune_dual.py --embedding gene2vec --embedding_file data/embeddings/gene2vec/double_outputs --tune_output_path /binf-isilon/renniegrp/vpx267/ucph_thesis/ray_results --tune_output_folder_name gene2vec --num_samples 100 --max_num_epochs 15
+
+    python tune_dual.py --embedding one-hot --tune_output_path /binf-isilon/renniegrp/vpx267/ucph_thesis/ray_results --tune_output_folder_name wo_batchnorm_dropout_dual --num_samples 200 --max_num_epochs 20
     """
     parser = ArgumentParser(
-        description="Running Ray Tuning"
+        description="Running Ray Tuning for dual output"
     )
     parser.add_argument("--embedding", default='one-hot',const='one-hot', nargs='?', 
                         choices=['one-hot', 'gene2vec'], 